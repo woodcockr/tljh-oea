20b19bb7ce860c9696a8ee0d88676314931f6237
Robert Woodcock <Robert.Woodcock@csiro.au>
2021-11-18 18:24:54 +1100

    Beginning of oea odc installation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # tljh-oea
+cd ~/dev/oea
+
+docker run \
+  --privileged \
+  --detach \
+  --name=tljh-dev \
+  --publish 12000:80 \
+  --mount type=bind,source=$(pwd),target=/srv/src \
+  tljh-systemd
+
+docker exec -it tljh-dev /bin/bash
+
+python3 /srv/src/the-littlest-jupyterhub/bootstrap/bootstrap.py --admin admin --plugin /srv/src/tljh-oea
--- tljh_oea.py
+++ tljh_oea.py
@@ -1,5 +1,50 @@
 from tljh.hooks import hookimpl
 
+
+@hookimpl
+def tljh_extra_user_pip_packages():
+    return [
+        '--extra-index-url=https://packages.dea.ga.gov.au',
+        'datacube[performance,s3]>=1.8.4.dev0',
+        'eodatasets3>=0.15.1',
+        'odc_algo[hdstats]',
+        'odc_ui',
+        'odc_index',
+        'odc_aws',
+        'odc_geom',
+        'odc_io',
+        'odc_aio',
+        'odc_dscache',
+        'odc_dtools',
+        'odc-apps-dc-tools',
+        'odc-apps-cloud',
+        'odc_ppt',
+        'datacube-stats',
+        '--no-binary=rasterio,Shapely,pygeos,netCDF4,pyproj,fc,hdstats,lmdb,lxml,numexpr,pyzmq,msgpack,ruamel.yaml.clib,zstandard'
+    ]
+
+# def   tljh_post_install():
+#     """
+#     Post install script to be executed after installation
+#     and after all the other hooks.
+#     This can be arbitrary Python code.
+#     """
+#     return [
+#     ]
+
+
+@hookimpl
+def tljh_config_post_install(config):
+    """
+    Set JupyterLab to be default
+    """
+    user_environment = config.get('user_environment', {})
+    user_environment['default_app'] = user_environment.get(
+        'default_app', 'jupyterlab')
+
+    config['user_environment'] = user_environment
+
+
 @hookimpl
 def tljh_extra_apt_packages():
     return [
@@ -19,7 +64,7 @@ def tljh_extra_apt_packages():
         'htop',
         'fish',
         'tig',
-        'git',
+        # 'git',
         'xz-utils',
         'openssh-client',
         'graphviz',
@@ -34,6 +79,29 @@ def tljh_extra_apt_packages():
         'unzip',
         'zip',
         'python3-dev',
-        'python3-pip',
-        'python3-venv'
+        # 'python3-pip',
+        # 'python3-venv'
+        'sqlite3',
+        'libjpeg-dev',
+        'libexpat-dev',
+        'libxerces-c-dev',
+        'libwebp-dev',
+        'libzstd-dev',
+        'libtiff5-dev',
+        'libpng-dev',
+        'libgif-dev',
+        'libdeflate-dev',
+        'libnetcdf-dev',
+        'libhdf4-alt-dev',
+        'libhdf5-serial-dev',
+        'libopenjp2-7-dev',
+        'libkml-dev',
+        'libatlas-base-dev',
+        'gfortran',
+        'libspatialindex-dev',
+        'libsfcgal-dev',
+        'libudunits2-dev',
+        'libgeos-dev',
+        'libgeos++-dev',
+        'libpq-dev'
     ]
